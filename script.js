b5896dd3d54317c54c97d2676d4d2312bf79e63c
jonasluis <144178301+jonasluis@users.noreply.github.com>
2024-01-16 22:54:14 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+
+  //substituir a imagem
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light1.png")
+
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar1.png")
+
+  }
+}
